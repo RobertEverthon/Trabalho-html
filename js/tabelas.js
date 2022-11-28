cf77fc502ab806bea7b11d01413187b2a55e9063
Robert Everthon <robert.everthon30@gmail.com>
2022-11-28 02:34:44 -0300

    Create tabelas.js

---
+++ js/tabelas.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector("#botaoSalvar")
+botaoSalvar.addEventListener("click", function(event){
+    event.preventDefault()
+
+    var formulario=document.querySelector("#form")
+})
