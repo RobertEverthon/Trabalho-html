01c7d00a9c27f5a1fff003ad2370b218b8af5ee2
Robert Everthon <robert.everthon30@gmail.com>
2022-11-28 11:33:29 -0300

    Delete ADM.html

--- js/tabelas.js
+++
@@ -1,6 +0,0 @@
-var btn = document.querySelector("#botaoSalvar")
-botaoSalvar.addEventListener("click", function(event){
-    event.preventDefault()
-
-    var formulario=document.querySelector("#form")
-})
