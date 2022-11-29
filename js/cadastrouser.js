f88910c530208db5f15830dd8c21ad124b6253cd
Diogo <unix980bits@tutamail.com>
2022-11-28 21:28:05 -0300

    ...

--- js/cadastrouser.js
+++ js/cadastrouser.js
@@ -12,7 +12,6 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     checkInputs();
-    checkCPF();
 });
 
 function checkInputs() {
@@ -81,7 +80,7 @@ function checkInputs() {
     });
 
     if (formIsValid) {
-    console.log("O formulário está 100% válido!");
+        console.log("O formulário está 100% válido!");
     }
 }
 
@@ -103,14 +102,45 @@ function checkEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
-function checkCPF() {
-    const cpfValue = cpf.value;
-
-    if (cpfValue === "") {
-        setErrorFor(cpf, "O CPF é obrigatório.");
-    } else if (cpfValue.length < 11) {
-        setErrorFor(cpf, "O CPF precisa ter 11 caracteres.");
+function validarCPF(el) {
+    if (!_cpf(el.value)) {
+        setErrorFor(cpf, "CPF inválido!");
     } else {
         setSuccessFor(cpf);
     }
 }
+
+function _cpf(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf == '') return false;
+    if (cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999")
+        return false;
+    add = 0;
+    for (i = 0; i < 9; i++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(9)))
+        return false;
+    add = 0;
+    for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(10)))
+        return false;
+    return true;
+}
+
